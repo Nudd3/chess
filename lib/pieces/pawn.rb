97a1c688aea9d946c1887f7d6926ee8c5cfb14d4
Noak Pettersson <noake12@gmail.com>
2022-05-20 03:14:18 +0200

    Pawn class is able to be added to board

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: false
+
 require_relative 'piece'
 # Class representing the pawns
 class Pawn < Piece
-  def initialize(color)
-    super
-    @icon = color == 'black' ? "\u265f" : "\u2659"
+  def initialize(color, position, board)
+    super(color, position, board)
+    @icon = color == 'white' ? "\u265f" : "\u2659"
   end
 
   def create_valid_moves
     current_x = position[:x]
     current_y = position[:y]
+  end
+
+  def to_s
+    @icon
   end
 end
 
