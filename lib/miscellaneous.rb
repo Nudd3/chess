7fd985e634aae3ae72f14b47d352f63058fcaa4f
Noak Pettersson <noake12@gmail.com>
2022-05-20 03:11:28 +0200

    Changed color of squares for better visualisation

--- lib/miscellaneous.rb
+++ lib/miscellaneous.rb
@@ -3,12 +3,14 @@
 # This module will be used for getting the different
 # icons needed for the chess game
 module Miscellaneous
-
+  # Currently using red color for gray & blue for black
   def gray2(piece = nil)
-    piece.nil? ? "\e[47m  #{piece} \e[0m" : "\e[47m #{piece} \e[0m"
+    # piece.nil? ? "\e[47m  #{piece} \e[0m" : "\e[47m #{piece} \e[0m"
+    piece.nil? ? "\e[41m  #{piece} \e[0m" : "\e[41m #{piece} \e[0m"
   end
 
   def black2(piece = nil)
-    piece.nil? ? "\e[40m  #{piece} \e[0m" : "\e[40m #{piece} \e[0m"
+    # piece.nil? ? "\e[40m  #{piece} \e[0m" : "\e[40m #{piece} \e[0m"
+    piece.nil? ? "\e[44m  #{piece} \e[0m" : "\e[44m #{piece} \e[0m"
   end
 end
