5148edccc959f2bfd025c8d9e3d84c6f8a6fc80f
Noak Pettersson <noake12@gmail.com>
2022-05-26 22:09:51 +0200

    Updated to use the actual colors as method names, methods are commented for information

--- lib/miscellaneous.rb
+++ lib/miscellaneous.rb
@@ -4,13 +4,25 @@
 # icons needed for the chess game
 module Miscellaneous
   # Currently using red color for gray & blue for black
-  def gray2(piece = nil)
-    # piece.nil? ? "\e[47m  #{piece} \e[0m" : "\e[47m #{piece} \e[0m"
+  def red(piece = nil)
+    # GRAY: piece.nil? ? "\e[47m  #{piece} \e[0m" : "\e[47m #{piece} \e[0m"
     piece.nil? ? "\e[41m  #{piece} \e[0m" : "\e[41m #{piece} \e[0m"
   end
 
-  def black2(piece = nil)
-    # piece.nil? ? "\e[40m  #{piece} \e[0m" : "\e[40m #{piece} \e[0m"
+  def blue(piece = nil)
+    # BLACK: piece.nil? ? "\e[40m  #{piece} \e[0m" : "\e[40m #{piece} \e[0m"
     piece.nil? ? "\e[44m  #{piece} \e[0m" : "\e[44m #{piece} \e[0m"
+  end
+
+  # Shows the possible moves on the board
+  # The possible_moves parameter is an array containing the
+  # coordinates that should be shown
+  # The board paramter is the board used in a game
+  def available_move_indicator(possible_moves, board)
+    possible_moves.each do |coordinate|
+      x = coordinate[0]
+      y = coordinate[1]
+      # Insert a circle with some color easily visible
+    end
   end
 end
