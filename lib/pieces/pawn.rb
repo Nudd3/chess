23d80f7652e4fcc5349914f5bc9ca7047d57a1ca
Noak Pettersson <noake12@gmail.com>
2022-05-27 03:17:54 +0200

    Pieces now have the coordinates

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -3,18 +3,35 @@
 require_relative 'piece'
 # Class representing the pawns
 class Pawn < Piece
-  attr_accessor :moves
+  attr_accessor :moves, :color, :position
 
-  def initialize(color, position, board)
-    super(color, position, board)
-    @icon = color == 'white' ? "\u265f" : "\u2659"
+  def initialize(color, coordinates, board)
+    super(color, coordinates, board)
+    @icon = color == 'black' ? "\u265f" : "\u2659"
     @moves = []
+    @board = board
   end
 
   def to_s
     @icon
   end
+
+  def generate_moves
+    # Check if pawn is in start position
+    # if coordinates[1] == 1 && color == 'white'
+    x = coordinates[0]
+    y = coordinates[1]
+    moves << [x + 1, y]
+  end
 end
+
+# Generate moves
+# Moves for pawn:
+# 1. Can only move 1 step forward, except first time where it's allowed to take two steps
+# 2. Can only "kill" another piece vertically
+# 3. 
+
+
 
 # Each piece's moves needs to be calculated from in here.
 # The king needs some kind of indicator to check if he's in check position
--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -1,45 +1,34 @@
 # frozen_string_literal: false
 
+# THE PIECES SHOULD CONTAIN THE COORDINATES
+# NOT THE SQUARES, SINCE THE SQUARES ARE STATIC
+# AND ALWAYS LOCATED AT THE SAME PLACE
+# THE PIECES ,HOWEVER, ARE NOT.
+# HENCE, THE PIECES SHOULD HAVE COORDINATES
+# THE SQUARES CAN BE ACCESSED THROUGH THE BOARD
+# SINCE THE BOARD HAS A GETTER
+
 # class representing pawns in a chess game
 class Piece
-  attr_accessor :position, :name, :color, :moves
+  attr_accessor :name, :color, :moves, :coordinates
 
-  # What does a pawn need?
-  # 1. A name
-  # 2. A color
-  # 3. Moves
-  #   - All move
-  #   - Possible moves
-
-  def initialize(color, position, board)
+  def initialize(color, board, coordinates)
+    @coordinates = coordinates
     @color = color
-    @moves = "hell"
-    
-    @position = position
+    @moves = []
     @board = board
   end
 
-  # move a piece to a new location
-  # since this class will be inherited from later on, this method is created here to work according to
-  # DRY, meaning I won't have to implement this same method for each of the different pieces.
   def move
-    # Calculate the possible squares a piece can reach
-    if possible_move?(destination)
-      # Remove piece from square (current location)
-      @position = destination
-      # Add piece to square (new location)
-    end
+    # todo
   end
 
   def possible_move?(destination)
-    new_x = destination[0]
-    new_y = destination[1]
-    !@board.board[new_x, new_y].taken?
+    # The moves of a piece will be calculated, however, the move itself will happen in the square class, between two squares
+    # todo
   end
 
   def to_s
     "Name: #{name}, color: #{color}"
   end
 end
-
-# FIND A PLACE TO PUT THE PHONE WHEN CODING!
